0be4c04cc1d00a4c0443dba20d74a9119bd29af7
JiLin <34726874+shakker2c@users.noreply.github.com>
2021-04-22 13:54:38 +0800

    Fixed Aspect algorithm error.

--- LineMesh.cs
+++ LineMesh.cs
@@ -1,6 +1,6 @@
-﻿using System.Text.RegularExpressions;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -17,15 +17,16 @@ public class Line {
     public Vector2 pivot = Vector2.zero;
     public Color color = Color.black;
 
-    public Line(Text text, LineType type, int index) {
+    public Line (Text text, LineType type, int index) {
         var characters = text.cachedTextGenerator.characters;
         var lines = text.cachedTextGenerator.lines[index];
         UICharInfo charInfo = characters[lines.startCharIdx];
-        position = new Vector2(charInfo.cursorPos.x, charInfo.cursorPos.y - text.cachedTextGenerator.lines[index].height * GetTypeWeight(type));
-        size = new Vector2(GetWidth(characters, lines.startCharIdx, characters.Count - 1),
+        float scale = GetRealPixelScale (text.transform.GetComponentInParent<CanvasScaler> ());
+        position = new Vector2 (charInfo.cursorPos.x, charInfo.cursorPos.y - text.cachedTextGenerator.lines[index].height * GetTypeWeight (type)) * scale;
+        size = new Vector2 (GetWidth (characters, lines.startCharIdx, characters.Count - 1) * scale,
             text.fontSize * 0.1f == 0 ? float.Epsilon : text.fontSize * 0.1f);
         color = text.color;
-        SetPivot(text.alignment);
+        SetPivot (text.alignment);
     }
 
     // public Line(Text text, LineType type, int index, int start, int length) {
@@ -37,7 +38,7 @@ public class Line {
     //     SetPivot(text.alignment);
     // }
 
-    float GetTypeWeight(LineType type) {
+    float GetTypeWeight (LineType type) {
         switch (type) {
         case LineType.upperline:
             return 0;
@@ -49,7 +50,7 @@ public class Line {
         }
     }
 
-    float GetWidth(IList<UICharInfo> characters, int start, int end) {
+    float GetWidth (IList<UICharInfo> characters, int start, int end) {
         float width = 0;
         int lastCharIdx = end - 1;
         for (int i = start; i < end; ++i) {
@@ -66,31 +67,37 @@ public class Line {
         return width;
     }
 
-    void SetPivot(TextAnchor anchor) {
+    float GetRealPixelScale (CanvasScaler cs) {
+        if (cs == null) return 1;
+        RectTransform rt = (RectTransform) cs.transform;
+        return rt.rect.width * cs.referenceResolution.y / rt.rect.height / cs.referenceResolution.x * (1 + rt.localScale[0]);
+    }
+
+    void SetPivot (TextAnchor anchor) {
         switch (anchor) {
         case TextAnchor.LowerLeft:
             pivot = Vector2.zero;
             break;
         case TextAnchor.LowerCenter:
-            pivot = new Vector2(0.5f, 0);
+            pivot = new Vector2 (0.5f, 0);
             break;
         case TextAnchor.LowerRight:
-            pivot = new Vector2(1, 0);
+            pivot = new Vector2 (1, 0);
             break;
         case TextAnchor.MiddleLeft:
-            pivot = new Vector2(0, 0.5f);
+            pivot = new Vector2 (0, 0.5f);
             break;
         case TextAnchor.MiddleCenter:
             pivot = Vector2.one * 0.5f;
             break;
         case TextAnchor.MiddleRight:
-            pivot = new Vector2(1, 0.5f);
+            pivot = new Vector2 (1, 0.5f);
             break;
         case TextAnchor.UpperLeft:
-            pivot = new Vector2(0, 1);
+            pivot = new Vector2 (0, 1);
             break;
         case TextAnchor.UpperCenter:
-            pivot = new Vector2(0.5f, 1);
+            pivot = new Vector2 (0.5f, 1);
             break;
         case TextAnchor.UpperRight:
             pivot = Vector2.one;
@@ -130,34 +137,34 @@ public class LineMesh : MonoBehaviour {
     //private const string underLineEvaluator = @"<un>.*</un>";
     //private const string underLineContentEvaluator = @"(?<=<un>).*(?=</un>)";
 
-    private void Awake() {
-        text = this.GetComponent<Text>();
-        lines = new List<Image>();
+    private void Awake () {
+        text = this.GetComponent<Text> ();
+        lines = new List<Image> ();
     }
 
     // Start is called before the first frame update
-    private void Start() {
-        UpdateParams();
+    private void Start () {
+        UpdateParams ();
         isInitialize = true;
     }
 
     // Update is called once per frame
-    private void Update() {
+    private void Update () {
         if (isInitialize) {
             if (type == LineType.none && currentType != type) {
                 currentType = type;
-                ClearLines();
+                ClearLines ();
             } else if (currentType != type || characterCount != text.cachedTextGenerator.characterCount ||
                 currentColor != text.color || currentFontStyle != text.fontStyle ||
                 currentAnchor != text.alignment || lineSpacing != text.lineSpacing) {
-                UpdateParams();
-                List<Line> lines = GetLines(type);
-                CreateLines(lines);
+                UpdateParams ();
+                List<Line> lines = GetLines (type);
+                CreateLines (lines);
             }
         }
     }
 
-    private void UpdateParams() {
+    private void UpdateParams () {
         characterCount = text.cachedTextGenerator.characterCount;
         currentType = type;
         currentColor = text.color;
@@ -166,58 +173,58 @@ public class LineMesh : MonoBehaviour {
         lineSpacing = text.lineSpacing;
     }
 
-    private List<Line> GetLines(LineType type) {
+    private List<Line> GetLines (LineType type) {
         if (type == LineType.none) return null;
-        List<Line> lineInfos = new List<Line>();
+        List<Line> lineInfos = new List<Line> ();
         for (int j = 0; j < text.cachedTextGenerator.lineCount; ++j) {
-            Line line = new Line(text, type, j);
-            lineInfos.Add(line);
+            Line line = new Line (text, type, j);
+            lineInfos.Add (line);
         }
         return lineInfos;
     }
 
-    private void ClearLines() {
+    private void ClearLines () {
         foreach (var line in lines)
-            Destroy(line.gameObject);
-        lines.Clear();
+            Destroy (line.gameObject);
+        lines.Clear ();
     }
 
-    private void CreateLines(List<Line> lineInfos) {
+    private void CreateLines (List<Line> lineInfos) {
         if (lineInfos == null) return;
-        ClearLines();
+        ClearLines ();
         for (int i = 0; i < lineInfos.Count; ++i) {
-            Image img = new GameObject().AddComponent<Image>();
-            img.transform.SetParent(text.transform, false);
+            Image img = new GameObject ().AddComponent<Image> ();
+            img.transform.SetParent (text.transform, false);
             img.name = $"line{i}";
             img.color = lineInfos[i].color;
             img.rectTransform.pivot = lineInfos[i].pivot;
-            lines.Add(img);
+            lines.Add (img);
 
-            Texture2D texture = new Texture2D((int)lineInfos[i].size[0], (int)lineInfos[i].size[1], TextureFormat.ARGB32, false);
-            var colors = texture.GetPixels();
+            Texture2D texture = new Texture2D ((int) lineInfos[i].size[0], (int) lineInfos[i].size[1], TextureFormat.ARGB32, false);
+            var colors = texture.GetPixels ();
             for (int j = 0; j < colors.Length; ++j)
                 colors[j] = img.color;
-            texture.SetPixels(colors);
-            texture.Apply();
+            texture.SetPixels (colors);
+            texture.Apply ();
 
-            img.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
-            img.SetNativeSize();
+            img.sprite = Sprite.Create (texture, new Rect (0, 0, texture.width, texture.height), Vector2.zero);
+            img.SetNativeSize ();
             img.rectTransform.sizeDelta = img.rectTransform.sizeDelta;
 
             float x = lineInfos[i].position.x;
             switch (text.alignment) {
-            case TextAnchor.MiddleCenter:
-            case TextAnchor.UpperCenter:
-            case TextAnchor.LowerCenter:
-                x = 0;
-                break;
-            case TextAnchor.MiddleRight:
-            case TextAnchor.UpperRight:
-            case TextAnchor.LowerRight:
-                x += lineInfos[i].size[0];
-                break;
+                case TextAnchor.MiddleCenter:
+                case TextAnchor.UpperCenter:
+                case TextAnchor.LowerCenter:
+                    x = 0;
+                    break;
+                case TextAnchor.MiddleRight:
+                case TextAnchor.UpperRight:
+                case TextAnchor.LowerRight:
+                    x += lineInfos[i].size[0];
+                    break;
             }
-            lines[i].rectTransform.anchoredPosition = new Vector2(x, lineInfos[i].position[1]);
+            lines[i].rectTransform.anchoredPosition = new Vector2 (x, lineInfos[i].position[1]);
         }
     }
 }
